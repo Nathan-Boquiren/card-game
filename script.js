20a4083ba7e59f8903429bc54ae2f8b90d72352a
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-07-09 11:50:09 -0400

    595 lines of code

--- script.js
+++ script.js
@@ -1,6 +1,162 @@
-// ===== Import Deck and Functions =====
+// ===== Classes =====
 
-import { deck, Player, Bot, playerNames } from "./logic.js";
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+    this.name = this.getName();
+    this.clr = this.getClr();
+  }
+
+  getName() {
+    if (this.rank < 11) return this.rank.toString();
+    const nameScheme = {
+      11: "Kack",
+      12: "Queen",
+      13: "King",
+      14: "Ace",
+    };
+    return nameScheme[this.rank];
+  }
+
+  getClr() {
+    if (this.suit === "Spades" || this.suit === "Clubs") {
+      return "Black";
+    } else if (this.suit === "Hearts" || this.suit === "Diamonds") {
+      return "Red";
+    }
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = this.buildDeck();
+    this.element = document.getElementById("deck");
+    this.cardsLeftElement = document.getElementById("deck-length");
+  }
+
+  buildDeck() {
+    let deck = [];
+    const suits = ["hearts", "spades", "diamonds", "clubs"];
+    for (let i = 2; i <= 14; i++) {
+      suits.forEach((suit) => {
+        deck.push(new Card(i, suit));
+      });
+    }
+    return deck;
+  }
+
+  dealHand() {
+    return [this.dealCard(), this.dealCard()];
+  }
+
+  dealCard() {
+    let randIndex = Math.floor(Math.random() * this.cards.length);
+    return this.cards.splice(randIndex, 1)[0];
+  }
+
+  populateCards(isFirstTime) {
+    if (!isFirstTime) this.element.innerHTML = "";
+    this.cards.forEach((card) => {
+      const cardElement = document.createElement("div");
+      cardElement.className = "card";
+      cardElement.style.backgroundImage = `url(card-images/${card.suit}-${card.rank}.png)`;
+      this.element.append(cardElement);
+    });
+
+    this.cardsLeftElement.innerText = this.cards.length;
+  }
+}
+
+class Player {
+  constructor(name, type) {
+    this.type = type;
+    this.name = name;
+    this.hand = deck.dealHand();
+    this.score = 0;
+    this.nameElement = document.getElementById(`${type}-name-wrapper`);
+    this.handElement = document.getElementById(`${type}-hand-container`);
+    this.scoreElement = document.getElementById(`${type}-score-wrapper`);
+  }
+
+  calculateScore() {
+    let score = 0;
+    this.hand.forEach((card) => {
+      if (card.rank === 14) {
+        score += score <= 10 ? 11 : 1;
+      } else {
+        score += card.rank >= 11 && card.rank < 14 ? 10 : card.rank;
+      }
+    });
+
+    this.score = score;
+
+    if (this.score === 21) return endMsg(`Congrats, ${this.name}`, "won");
+    if (this.score > 21) return endMsg(`Oh. ${this.name}`, "busted");
+  }
+
+  updateScoreElement() {
+    this.calculateScore();
+    this.scoreElement.innerText = `${this.score}`;
+  }
+
+  hit() {
+    this.hand.push(deck.dealCard());
+    updateDom();
+  }
+
+  renderName() {
+    this.nameElement.innerText = this.name;
+  }
+
+  renderHand(isFirstTime) {
+    this.handElement.innerHTML = "";
+    this.hand.forEach((c) => {
+      const cardElement = document.createElement("div");
+      cardElement.className = "card";
+      cardElement.style.backgroundImage = `url(card-images/${c.suit}-${c.rank}.png)`;
+      this.handElement.append(cardElement);
+    });
+
+    this.styleHandElement();
+
+    if (isFirstTime) this.animateHandDeal();
+  }
+
+  styleHandElement() {
+    const cards = this.handElement.querySelectorAll(".card");
+    const cardWidth = 178.5714286;
+    const overlap = 40;
+    const newContainerWidth = cardWidth + (cards.length - 1) * overlap;
+    this.handElement.style.width = `${newContainerWidth}px`;
+    cards.forEach((card, i) => (card.style.left = `${i * overlap}px`));
+  }
+
+  animateHandDeal() {
+    this.handElement.classList.add("show-cards");
+    setTimeout(() => this.handElement.classList.remove("show-cards"), 300);
+  }
+}
+
+class Bot extends Player {
+  constructor(name, type) {
+    super(name, type);
+  }
+
+  turn() {
+    this.handElement.classList.add("remove-after");
+    this.calculateScore();
+    if (this.score < 17) {
+      this.hit();
+      setTimeout(() => {
+        this.turn();
+        updateDom();
+      }, 500);
+    } else if (this.score >= 17 && this.score < 21) {
+      setTimeout(compareScores, 500);
+    }
+  }
+}
 
 // ===== DOM Elements =====
 
@@ -12,6 +168,29 @@ const msgWrapper = document.getElementById("msg-wpr");
 
 // ===== Global Variables =====
 
+const playerNames = [
+  "Nathan",
+  "Brianna",
+  "Brennan",
+  "Andrew",
+  "Jacob",
+  "Kole",
+  "Jacob",
+  "Daniel",
+  "Katie",
+  "Summer",
+  "Aria",
+  "Hunter",
+  "Hannah",
+  "Gaines",
+  "Ben",
+  "Donald Trump",
+  "Joe Biden",
+  "Jack Black",
+  "Taylor Swift",
+];
+
+let deck = new Deck();
 let user;
 let bot;
 let players = [];
@@ -66,13 +245,13 @@ choiceBtns.forEach((btn) => {
 
 // ===== Win and Lose Functions =====
 
-export function endMsg(player, status) {
+function endMsg(player, status) {
   msgWrapper.innerHTML = status ? `${player} ${status}!` : player;
   msgContainer.classList.add("show");
 }
 // ===== UPDATE DOM =====
 
-export function updateDom() {
+function updateDom() {
   deck.populateCards(false);
   players.forEach((p) => p.renderHand(false));
   user.updateScoreElement();
@@ -80,7 +259,7 @@ export function updateDom() {
 
 // ===== Compare Scores =====
 
-export function compareScores() {
+function compareScores() {
   if (user.score === bot.score) return endMsg("It's a tie!", null);
 
   const winner = user.score > bot.score ? user : bot;
